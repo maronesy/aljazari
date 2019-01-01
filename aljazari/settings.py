42885a360f4dd4d2f027885a9a5891210542fc96
maronesy <souhayl@gmail.com>
2019-01-01 22:38:07 +0300

    addes STATOC_ROOT

--- aljazari/settings.py
+++ aljazari/settings.py
@@ -118,3 +118,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Heroku settings
+
+# ROOT_PATH = os.path.dirname(__file__)
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    ALLOWED_HOSTS = ['*']
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__)),
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
